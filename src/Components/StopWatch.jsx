589d712d5b33ebd60d21baf281adeceedd99a1a9
Rakesh Patel <rakesh246808@gmail.com>
2024-08-26 16:06:16 +0530

    added stop watch functionalty

--- src/Components/StopWatch.jsx
+++ src/Components/StopWatch.jsx
@@ -1,102 +1,101 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import "../scss/style.css";
 
-let time = `00`;
+let time = 0;
 
-function StopWatch()
-{
-    const [miliSeconds, miliUpdate] = useState(time);
-    const [seconds, secUpdate] = useState(time);
-    const [minutes, minUpdate] = useState(time);
-    const [hours, hourUpdate] = useState(time);
+function StopWatch() {
+  const [miliSeconds, miliUpdate] = useState(time);
+  const [seconds, secUpdate] = useState(time);
+  const [minutes, minUpdate] = useState(time);
+  const [hours, hourUpdate] = useState(time);
 
-    function start()
+  const [Running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let allTime = null;
+
+    if (Running) 
     {
-        let milf = 0;
-        let mils = 0;
-       const mili = setInterval(() => 
-        {
-            if(mils>9)
-            {
-                mils=0;
-                milf++;
-            }
-            if(milf>5)
-            {
-                milf=0;
-            }
-            miliUpdate(`${milf}${mils}`);
-            mils++;
-        },1);
-
-       let secf=0;
-       let secs=0;
-       const sec = setInterval(() => 
-        {
-            if(secs>9)
-            {
-                secs=0;
-                secf++;
-            }
-            if(secf>5)
-            {
-                secf=0;
-            }
-            secUpdate(`${secf}${secs}`);
-            secs++;
-        },1000);
-       let minf=0;
-       let mins=0;
-       const min = setInterval(() => 
-        {
-            if(mins>9)
-            {
-                mins=0;
-                minf++;
-            }
-            if(minf>5)
-            {
-                minf=0;
-            }
-            minUpdate(`${minf}${mins}`);
-            mins++;
-        },60000);
-
-       let hourf=0;
-       let hours=0;
-       const hour = setInterval(() => 
-        {
-            if(hours>9)
-            {
-                hours=0;
-                hourf++;
-            }
-            if(hourf>5)
-            {
-                hourf=0;
-            }
-            hourUpdate(`${hourf}${hours}`);
-            hours++;
-        },3600000);
-
+        allTime = setInterval(() => {
+            miliUpdate((prevMili) => {
+                if(prevMili === 10)
+                {
+                    secUpdate((prevSeconds) => {
+                        if(prevSeconds === 59)
+                        {
+                            minUpdate((prevMin) => {
+                                if(prevMin === 59)
+                                {
+                                    hourUpdate((prevHour) => prevHour+1)
+                                    return 0;
+                                }
+                                else
+                                {
+                                    return prevMin+1;
+                                }
+                                    
+                            })
+                            return 0;
+                        }
+                        else
+                        {
+                            return prevSeconds+1;
+                        }
+                    })
+                    return 0;
+                }
+                else
+                {
+                    return prevMili+1;
+                }
+            })
+        },100)
     }
 
+   
+       return () => clearInterval(allTime);
+    
+  }, [Running]);
 
-    return (
-        <div className="stopWatch-container">
-            <div className="timer-container">
-               <span className="timer">{hours} : </span>
-               <span className="timer">{minutes} : </span>
-               <span className="timer">{seconds} : </span>
-               <span className="timer">{miliSeconds}</span>
-            </div>
-            <div className="btn-container">
-                <button className="btn" onClick={start} id="startBtn">Start</button>
-                <button className="btn" onClick={stop} id="stopBtn">Stop</button>
-                <button className="btn" id="resetBtn">Reset</button>
-            </div>
-        </div>
-    );
+  function startTimer() {
+    setRunning(true);
+  }
+
+  function stopTimer() {
+    setRunning(false);
+}
+
+function resetTimer() {
+    setRunning(false);
+    miliUpdate(0);
+    secUpdate(0);
+    minUpdate(0);
+    hourUpdate(0);
+  }
+
+  return (
+    <div className="stopWatch-container">
+      <div className="timer-container">
+        <span className="timer">
+        {hours<10? "0"+hours:hours} :
+        {minutes<10? " 0"+minutes:" "+minutes} :    
+        {seconds<10? " 0"+seconds:" "+seconds} :
+        {miliSeconds<10? " 0"+miliSeconds:miliSeconds} 
+        </span>
+      </div>
+      <div className="btn-container">
+        <button className="btn" onClick={startTimer} id="startBtn">
+          Start
+        </button>
+        <button className="btn" onClick={stopTimer} id="stopBtn">
+          Stop
+        </button>
+        <button className="btn" onClick={resetTimer} id="resetBtn">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default StopWatch;
